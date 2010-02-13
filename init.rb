303257d7598a61666a26227e9e8243b6e939544e
chrislloyd <christopher.lloyd@gmail.com>
2010-02-14 10:56:40 +1100

    Cleanups and fix Sass load path.

--- init.rb
+++ init.rb
@@ -7,15 +7,9 @@ end
 
 Bundler.require :default
 
-# require File.join(File.dirname(__FILE__),'vendor','gems','environment')
-# Bundler.require_env
-
 configure do
-
-  require 'sass/plugin'
-  Sass::Plugin.options[:load_paths] = [Sinatra::Application.views]
-
   ENV['TZ'] = 'Australia/Sydney'
 
   set :app_file => 'poacher.rb'
+  set :sass => {:load_paths => [File.join(Sinatra::Application.public,'sass')]}
 end
