16e0ffd7cc6030b802f3292e8940e45d3e7e76e3
Chris Lloyd <christopher.lloyd@gmail.com>
2010-05-19 00:11:01 +1000

    String migration.

--- database.rb
+++ database.rb
@@ -1,7 +1,7 @@
 migration 'create the hearts table' do
   database.create_table :hearts do
     primary_key :id
-    string      :path
+    String      :path
     text        :src
     timestamp   :created_at, :null => false
     index       :path
