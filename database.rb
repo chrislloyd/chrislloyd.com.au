8bc5585390304b8974f6915c88a44921b9dcb270
Chris Lloyd <christopher.lloyd@gmail.com>
2010-05-19 00:24:56 +1000

    Migration style update.

--- database.rb
+++ database.rb
@@ -2,8 +2,8 @@ migration 'create the hearts table' do
   database.create_table :hearts do
     primary_key :id
     String      :path
-    text        :src
-    timestamp   :created_at, :null => false
+    String      :src, :text => true
+    DateTime    :created_at, :null => false
     index       :path
   end
 end
