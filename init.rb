1597b4ca898451e482a4a472cdd98bbe83b85ee4
Chris Lloyd <christopher.lloyd@gmail.com>
2010-09-08 14:18:46 +0200

    Try setting Encoding.default_external.

--- init.rb
+++ init.rb
@@ -10,6 +10,8 @@ Bundler.require :default, ENV['RACK_ENV'].to_sym
 require './database'
 
 configure do
+  Encoding.default_external = 'UTF-8' if String.new.respond_to?(:encoding)
+
   set :app_file => 'poacher.rb'
   set :haml => {:format => :html5, :encoding => 'utf-8'}
   set :sass => {:load_paths => [File.join(Sinatra::Application.public,'sass')]}
